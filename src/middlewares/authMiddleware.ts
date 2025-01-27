0e4ec99e5ced57638ab4e4c6033be90edf0869ad
wener <wener@greenn.com.br>
2025-01-27 15:15:20 -0300

    fix remove middleware

--- src/middlewares/authMiddleware.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyRequest, FastifyReply } from 'fastify';
-
-// Middleware simples de log
-export const testMiddleware = async (request: FastifyRequest, reply: FastifyReply) => {
-  console.log(`Request method: ${request.method}, Request URL: ${request.url}`);
-  
-  // Não há necessidade de chamar 'next()', o Fastify continua automaticamente.
-};
