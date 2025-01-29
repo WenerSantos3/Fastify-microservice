2b7f8714c78216a7990bac57c387e6fe9845da2f
joao henryque <joao.cunha@greenn.com.br>
2025-01-29 09:09:35 -0300

    fix: swagger

--- src/modules/globalSettings/swagger/getGlobalSettingsKeys.ts
+++ src/modules/globalSettings/swagger/getGlobalSettingsKeys.ts
@@ -1,25 +1,31 @@
 export default {
-    schema: {
-      description: "Busca o valor de uma key no global-settings",
-      tags: ["Global Settings"],
-      example: {
-        key: "SALES_BLOCK_DAYS",  // Exemplo de chave
+  schema: {
+    description: "Busca o valor de uma key no global-settings",
+    tags: ["Global Settings"],
+    body: {
+      type: "object",
+      required: ["key"],
+      properties: {
+        key: { type: "string" },
       },
-      body: {
+      examples: [
+        {
+          key:"SALES_BLOCK_DAYS",
+        },
+      ],
+    },
+    response: {
+      200: {
         type: "object",
-        required: ["key", "value"],
         properties: {
-          key: { type: "string" },
+          value: { type: "string" },
         },
-      },
-      response: {
-        200: {
-          type: "object",
-          properties: {
-            value: { type: "string" },
+        examples: [
+          {
+            value:"10",
           },
-        },
+        ],
       },
     },
-  };
-  
+  },
+};
